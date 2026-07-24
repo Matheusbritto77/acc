2763eae9807bd17d53a44bb37d31005d1761d39b
matheus  brito <matheusbritto@MacBook-Air-de-matheus.local>
2026-07-24 16:58:50 -0300

    Limit vocation list to base classes

--- app/Model/Functions/Player.php
+++ app/Model/Functions/Player.php
@@ -26,10 +26,6 @@ use App\Model\Functions\Guilds as FunctionsGuilds;
                 2 => 'Druid',
                 3 => 'Paladin',
                 4 => 'Knight',
-                5 => 'Master Sorcerer',
-                6 => 'Elder Druid',
-                7 => 'Royal Paladin',
-                8 => 'Elite Knight',
                 9 => 'Monk',
                 10 => 'Exalted Monk',
             ];
