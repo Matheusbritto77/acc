c9364022b6e2e16b41e92de84e215087d8681d45
matheus  brito <matheusbritto@MacBook-Air-de-matheus.local>
2026-07-27 15:03:37 -0300

    Update Windows download link to v4.2.3

--- app/Controller/Pages/Downloads.php
+++ app/Controller/Pages/Downloads.php
@@ -13,7 +13,7 @@ use \App\Utils\View;
 use App\Model\Entity\ServerConfig as EntityServerConfig;
 
 class Downloads extends Base{
-    private const DEFAULT_WINDOWS_DOWNLOAD = 'https://github.com/Matheusbritto77/client/releases/download/v4.2.2/OTClient-windows-x64.zip';
+    private const DEFAULT_WINDOWS_DOWNLOAD = 'https://github.com/Matheusbritto77/client/releases/download/v4.2.3/OTClient-windows-x64.zip';
     private const DEFAULT_LINUX_DOWNLOAD = 'https://github.com/Matheusbritto77/client/releases/tag/v4.2.2';
     private const DEFAULT_MACOS_DOWNLOAD = 'https://astarot.online/resources/client-updater/stable/packages/OTClient-macos-arm64.zip';
 
